a5683b0f739dfffc7a2499ce41434a220c18a8cc
elpfian <41032297+elpfian@users.noreply.github.com>
2018-07-09 22:38:56 -0500

    Update sigma.m

--- sigma.m
+++ sigma.m
@@ -1,7 +1,14 @@
-function a = sigma( X )
-% Calculates logsig
-% X = column vector
+function a = sigma( X, dFlag )
+% Calculates logsig or derivative
+% X = matrix
+% dFlag = 0 indicates function, 1 indicates take derivative
  
+% Sigma
 a = 1./(1 + exp(-X) );
-    
+ 
+% Sigma derivative
+if dFlag
+    a = a.*( 1-a );  
+end
+ 
 end
