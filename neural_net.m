8e4b4d774ca6e259785aa6f41eaaa5a30e412b66
elpfian <41032297+elpfian@users.noreply.github.com>
2018-07-09 22:31:17 -0500

    Update neural_net.m

--- neural_net.m
+++ neural_net.m
@@ -1,115 +1,227 @@
-function [wCur, bCur] = neural_net( inputs, targets, nodeLayers, numEpochs, batchSize, eta )
-% Learns a neural network. Writes performance to file 'results.txt'. 
-% No output returned.
+function [wCur, bCur, acc, cost] = neural_net( inputs, targets,...
+    nodeLayers, numEpochs, batchSize, split, eta, mu, lambda,...
+    transF, costF, wStart, bStart)
+% Learns a neural network. Returns learned weights(wCur) and biases(bCur)
+% and accuracy(acc) & cost(cost) per epoch for training, 
+% test and validation. Prints results for each epoch.
 %
-% FUNCTION VARIABLES
+%% FUNCTION DATA
+% INPUT VARIABLES
 % inputs = matrix, col for each example and row for each input feature
 % targets = matrix, col for each example and row for each output feature
 % nodeLayers = vector, # of nodes in each layer
 % numEpochs = scalar, # of epochs to run
 % batchSize = scalar, # of examples in mini-batch
+% split = vector, proportion of examples for train, test and validation
 % eta = scalar, learning rate
+% mu = scalar, momentum coefficient
+% lambda = scalar, coefficient for L2 weight regularization
+% transF = transfer function
+% costF = cost function
+% wStart (optional) = cell, initial weight matrices
+% bStart (optional) = cell, initial bias matrices
 %
-% REQUIRED FUNCTION FILES: init_weights, init_biases, back_prop, sigma,
-% meanSqErr
+% OUTPUT VARIABLES
+% wCur = cell, learned weight matrices for each layer of network
+% bCur = cell, learned biase matrices for each layer of network
+% acc = matrix, col for accuracy per epoch for rows train, test & validation
+% cost = matrix, col for cost per epoch for rows train, test & validation
+%
+% REQUIRED FUNCTION FILES: init_better_network, feed_forward,
+% one_epoch (REQUIRES back_prop_SGD), cost_acc, screen_print, 
+% COST FUNCTIONS(log_like, quad_cost, x_entropy), 
+% TRANSFER FUNCTIONS(relu, sigma, softmax, tan_h)
  
-% INTERMEDIATE VARIABLES
-trainSize = size( inputs, 2 );
-netDepth = size( nodeLayers, 2);
-numBatches = ceil( trainSize/batchSize );
+%% INPUT CHECKS
+transName = func2str(transF);
+costName = func2str(costF);
  
-% INITIALIZE weights and biases
-wCur = init_weights( nodeLayers );
-bCur = init_biases( nodeLayers );
+% Valid number of cases in inputs vs targets
+if size( inputs, 2 ) ~= size( targets, 2 )
+    error('Input and targets must have same number of cases.');
+    return                              % EXIT NEURAL_NET
+% Valid network size for inputs, targets
+elseif size( inputs, 1) ~= nodeLayers(1)...
+        || size( targets, 1 ) ~= nodeLayers(end);   
+    error('Incorrect network size.');
+    return                              % EXIT NEURAL_NET
+% Valid transfer function
+elseif isempty( strfind( 'sigma,relu,tan_h,softmax', transName ) )
+    error('Invalid transfer function.');
+    return                              % EXIT NEURAL_NET
+% Valid cost function
+elseif isempty( strfind( 'log_like,quad_cost,x_entropy', costName ) )
+    error('Invalid cost function.');
+    return                              % EXIT NEURAL_NET
+% All positive parameters
+elseif any([numEpochs, batchSize, eta, lambda, mu] < 0 )
+    error('Parameters cannot be negative.');
+    return                              % EXIT NEURAL_NET
+% If supplied starting weights or biases, supplied both    
+elseif xor( exist('wStart', 'var'), exist('bStart', 'var') )
+    error('If supplying starting weights and biases, must supply both.');
+    return                              % EXIT NEURAL_NET
+end
  
-% INITIALIZE loop conditionals
-countEpoch = 1;     % initialize epoch counter
-acc = 0;            % initialize accuracy
+%% INITIALIZATIONS
+% Accuracy & cost
+acc = zeros(3, numEpochs);  % rows for train, test, validation
+cost = zeros(3, numEpochs); % rows for train, test, validation
  
-% OPEN OUTPUT FILE
-fName = 'results.txt';
-fileID = fopen(fName, 'w');
+% Stopping conditions
+accTrn = 0;      % initialize training accuracy
+stop = 10;       % stop if no improvement in this # of epochs
+countEpoch = 1;  % epoch counter
  
-% MAIN LOOP
-% Stop when numEpochs completed or accuracy = 100%
-while (countEpoch <= numEpochs) && (acc < 1);       % START WHILE loop
-   
-    % Index to randomize training set order for each epoch
-    shuffInd = randperm( trainSize );
+% Intermediate variables
+netDepth = size(nodeLayers, 2);
+numCases = size( inputs, 2 );
  
-    batchStart = 1;     % initialize batchStart pointer
+% Weights, biases, and change in weights/biases
+if ~exist('wStart', 'var') && ~exist('bStart', 'var') 
+    % No weights/biases supplied
+    [wCur, bCur, dwCur, dbCur] = init_better_network( nodeLayers );
     
-    % LOOP OVER EPOCH
-    % Stop after all records used
-    for batch = 1:numBatches        % START epoch
-        
-        % GENERATE MINI-BATCH
-        batchEnd = batchStart + batchSize -1;   % create batchEnd pointer
- 
-        % select vector of indices batchInd from shuffled index
-        if batchEnd > size( shuffInd );     % last batch
-            batchInd = shuffInd( batchStart: end );
-        else                                % all other batches
-            batchInd = shuffInd( batchStart:( batchEnd ) );
-        end
-        
-        % select inputs, targets for batch using batchInd
-        x_batch = inputs( :, batchInd );    % all rows, shuffled cols
-        y_batch = targets( :, batchInd );   % all rows, shuffled cols
- 
-        batchStart = batchStart + batchSize;% increment batchStart pointer
-        
-        % BACKPROPAGATION
-        % Run for each mini-batch, returns [new_weights, new_biases]
-        % Use "size(batchInd, 2)" instead of batch size in case
-        %   last batch is different size
-        
-        temp = {};      % clear temp result
-        
-        [temp{1:2}] = back_prop(x_batch, y_batch,...
-            wCur, bCur, netDepth, eta, size(batchInd, 2) );
-        
-        wCur = temp{1};
-        bCur = temp{2};
-                
-    end                             % END epoch
+else % Supplied weights/biases
+    wCur = wStart;
+    bCur = bStart;
     
-    
-    % GENERATE NETWORK OUTPUT 
-    % For entire training set with updated weights/biases
-    % Initialize activation and weighted inputs
-    activation = sigma(inputs);
-    zInput = [];
+    % Initialize change in weight/bias
+    dwCur = cell( 1, netDepth);
+    dbCur = cell( 1, netDepth);
  
-    % Feedforward over entire network. Final activation = network output.
-    % Can overwrite activation, zInput as only need final activation
+    % cell 1 not used for calculation, kept for indexing
     for i = 2:netDepth
-        
-        % calculate weighted input for layer i
-        zInput = wCur{i}*activation + repmat( bCur{i}, 1, trainSize );
-        
-        % calculate activation for layer i
-        activation = sigma( zInput );
+        dwCur{i} = zeros( size(wCur{i}) );
+        dbCur{i} = zeros( size(bCur{i}) );
+    end
+end     % End weights, biases, etc.
  
+%% GENERATE TRAIN/TEST/VALIDATION SETS
+% Create randomized index
+splitInd = randperm( numCases ); 
+ 
+% Use "split" variable to get size of each subset
+numVal = round( split(3)*numCases );
+numTst = round( split(2)*numCases );
+numTrn = numCases - numTst - numVal; % In case of uneven split
+ 
+% Assign inputs using randomized index
+inputTrn = inputs( :, splitInd(1:numTrn) );
+inputTst = inputs( :, splitInd( (numTrn+1):(numTrn+numTst) ) );
+inputVal = inputs( :, splitInd( (numTrn+numTst+1):end ) );
+ 
+% Assign targets using randomized index
+targetTrn = targets( :, splitInd(1:numTrn) );
+targetTst = targets( :, splitInd( (numTrn+1):(numTrn+numTst) ) );
+targetVal = targets( :, splitInd( (numTrn+numTst+1):end ) );
+ 
+%% MAIN LOOP
+% Print parameters for reference
+fprintf('\nEps:%d, Batch:%d, eta:%.2f, ', numEpochs, batchSize, eta)
+fprintf('Trans: %s, Cost: %s, Mom:%.1f, Reg:%.1f\n',...
+    transName, costName, mu, lambda);
+                               
+% Stop when numEpochs completed or training accuracy = 100%
+while (countEpoch <= numEpochs) && (accTrn < 1);    % START WHILE loop
+        
+    %% LEARN NETWORK for one epoch using training data
+    [wCur, bCur, dwCur, dbCur] =...
+    one_epoch( inputTrn, targetTrn, wCur, bCur, dwCur, dbCur,...
+    nodeLayers, batchSize, eta, lambda, mu, transF, costName);
+    
+    %% GENERATE NETWORK OUTPUT, COST, ACCURACY for entire epoch on each set
+    
+    % CALCULATE weight minimization term for L2 Regularization
+    % sum of (all weights squared)
+    totWSq = 0;
+    for i = 2:netDepth     % wCur{1} is empty
+        totWSq = totWSq + sum( sum(wCur{i}.^2) );
+    end
+    regTerm = (lambda/2/numCases)*totWSq;
+ 
+    % TRAIN
+    % Output
+    [active, ~ ] =...
+        feed_forward( inputTrn, wCur, bCur, netDepth, transF, costName );
+    output = active{netDepth};
+    
+
+
+    % Correct, cost, accuracy
+    [corTrn, costTrn, accTrn] = ...
+        cost_acc( output, targetTrn, numTrn, costF, regTerm );
+ 
+    % TEST
+    if numTst == 0  % If no test set from split
+        corTst = 0;
+        costTst = 0;
+        accTst = 0;
+    else            % Test set designated
+        % Output
+        [active, ~ ] =...
+            feed_forward( inputTst, wCur, bCur, netDepth, transF, costName );
+        output = active{netDepth};
+        
+        % Correct, cost, accuracy
+        [corTst, costTst, accTst] = ...
+            cost_acc( output, targetTst, numTst, costF, regTerm );
+    end             % End TEST
+    
+    % VALIDATION
+    if numVal == 0      % If no validation set from split
+        corVal = 0;
+        costVal = 0;
+        accVal = 0;
+    else                % Validation set designated
+        % Output
+        [active, ~ ] =...
+            feed_forward( inputVal, wCur, bCur, netDepth, transF, costName );
+        output = active{netDepth};
+        
+        % Correct, cost, accuracy
+        [corVal, costVal, accVal] = ...
+            cost_acc( output, targetVal, numVal, costF, regTerm );
+ 
+        % Update best validation cost for stopping condition
+        % 1st time through or current cost less than previous min
+        if  ~exist('costValMin', 'var') || (costVal < costValMin)
+            costValMin = costVal;
+        end
+ 
+    end                 % End VALIDATION
+    
+    % PRINT
+    if numEpochs < 100
+        screen_print(countEpoch, costTrn, corTrn, numTrn, accTrn,...
+            costTst, corTst, numTst, accTst, costVal, corVal, numVal, accVal)
+        
+    elseif (numEpochs > 100) &&...      % Print every 10 & last epoch
+            ( (mod(countEpoch,10) == 0) || (countEpoch == numEpochs) )
+        screen_print(countEpoch, costTrn, corTrn, numTrn, accTrn,...
+            costTst, corTst, numTst, accTst, costVal, corVal, numVal, accVal)
+    end
+    
+    % UPDATE ACCURACY AND COST matrices
+    acc(:, countEpoch) = [accTrn; accTst; accVal];
+    cost(:, countEpoch) = [costTrn; costTst; costVal];
+    
+    %% EARLY STOPPING
+    % At least stop # of epochs has passed and minimum validation cost
+    % has not occurred in last stop # of epochs
+    if exist('costValMin', 'var') && (countEpoch > stop) ...
+            && all( cost(3, (countEpoch+1-stop):countEpoch ) > costValMin );
+        fprintf('\n\nEarly stopping - validation cost has not improved in %d epochs.\n', stop);
+        return      % EXIT NEURAL_NET
     end
         
-    % CALCULATE AND PRINT
-    % MSE, Correct Classifications, Accuracy
-    mse = meanSqErr( activation, targets );
-    correct = sum( all( targets == round(activation), 1 ) );
-    acc = correct / trainSize;
-    
-    % PRINT to fileID (named above while loop)
-    style = 'Epoch %3d \t MSE: %5.4f \t Correct: %d / %d \t Acc: %.3f\r\n';
-    fprintf(fileID, style, countEpoch, mse, correct, trainSize, acc);
-    
-    
     % INCREMENT epoch counter
     countEpoch = countEpoch +1;
  
 end                                             % END WHILE
  
-% Close results file
-fclose(fileID);
+if accTrn == 1
+    fprintf('\n\nEarly stopping - 100%% accuracy acheived!\n');
+end
  
 end     % END function
